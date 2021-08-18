7bcf1b895ae0732298f8b05c0f1ca029bc26626b
DavidBartTobi <82588072+DavidBartTobi@users.noreply.github.com>
2021-08-18 22:26:44 +0300

    Delete matrix_Multiplication_Entry.py

--- matrix_Multiplication_Entry.py
+++
@@ -1,9 +0,0 @@
-from utilities import gui_utilities
-
-class Matrix_Multiplication_Entry:
-
-    def __init__(self, top):
-
-        self.top = top
-        gui_utilities.Window_Style(self.top, "הזן גדלי מטריצות", "300x150")
-
